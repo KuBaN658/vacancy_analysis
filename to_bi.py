2c7b3da203eb26b20a1fa8ec79f2bea1a6a958e3
KuBaN23_ <garkovski_dmitri@mail.ru>
2024-05-15 16:11:58 +0300

    реализовать предобработку для bi

---
+++ to_bi.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import requests
+from utils import (
+    calc_experience,
+    calc_salary_bin, 
+    calc_salary_num,
+    convert_salary, 
+    lemmatize_corpus, 
+    calc_skills, 
+    calc_skills_from_description)
+
+da = pd.read_csv('data/da.csv', parse_dates=['published_at'])
+ds = pd.read_csv('data/ds.csv', parse_dates=['published_at'])
+
+# Создаем переменные
+da['name_type'] = 'da'
+ds['name_type'] = 'ds'
+
+# объеденяем таблицы
+vacancies = pd.concat((da, ds))
+
+grid = (vacancies.name.str.lower().str.contains(r'data scien')
+    & (vacancies.name.str.lower().str.contains(r'analyst')
+    | vacancies.name.str.lower().str.contains(r'аналитик')) 
+    & (~vacancies.name.str.lower().str.contains(r'видеоаналитика')))
+
+vacancies = vacancies[~grid].sort_values(by='id')
+
+vacancies['published_date'] = vacancies.published_at.dt.date
+
+vacancies.drop_duplicates(
+    subset=['name', 'employer', 'department', 'area', 'description'],
+    keep=False, inplace=True
+)
+
+vacancies.experience = vacancies.experience.map(calc_experience)
+
+vacancies['description_lemmatized'] = lemmatize_corpus(vacancies.description)
+
+# Создаем новую колонку 'skills_from_key_skills' в DataFrame 'vacancies'
+# В этой колонке для каждой вакансии будет храниться уникальный набор навыков,
+# приведенный к нижнему регистру
+vacancies['skills_from_key_skills'] = (
+    vacancies['key_skills'].map(
+        lambda x: ', '.join(set([skill[1:-1] for skill in x[1:-1].lower().split(', ')])))
+)
+
+# Преобразуем колонку 'skills_from_key_skills' в список для дальнейшей обработки
+skills_data = vacancies['skills_from_key_skills'].to_list()
+
+# Инициализируем словарь для подсчета частоты встречаемости каждого навыка
+counter = {}
+
+# Проходим по всем навыкам в списке 'skills_data'
+for sequence in skills_data:
+    # Разделяем строку с навыками на уникальные элементы и преобразуем в множество
+    skills = set(sequence.split(', '))
+    # Подсчитываем количество вхождений каждого навыка
+    for skill in skills:
+        counter[skill] = counter.get(skill, 0) + 1
+
+# Фильтруем словарь 'counter', оставляя только те навыки, которые встречаются более 20 раз
+counter = {k: v for k, v in counter.items() if v > 10}
+
+# Сортируем словарь 'counter' по убыванию частоты встречаемости навыков
+counter = dict(sorted(counter.items(), key=lambda item: item[1], reverse=True))
+
+del counter['']
+del counter['анализ данных']
+del counter['data analysis']
+del counter['machine learning']
+del counter['аналитика']
+del counter['data science']
+del counter['ml']
+del counter['аналитические исследования']
+del counter['машинное обучение']
+del counter['работа с большим объемом информации']
+del counter['it']
+
+del counter['ms excel']
+del counter['ms powerpoint']
+del counter['ms power bi']
+
+# сохраняем скиллы во множестве и добавляем нужные элементы
+SKILLS = set(counter.keys()).union({'excel', 'powerpoint', 'power bi'})
+
+vacancies['skills_from_description'] = vacancies['description_lemmatized'].apply(calc_skills_from_description, skills=SKILLS)
+vacancies['skills'] = vacancies.apply(calc_skills, axis=1)
+
+vacancies['salary_num'] = vacancies.apply(calc_salary_num, axis=1)
+
+# получаем курс валют ЦБ
+CB = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
+vacancies['salary_rub'] = vacancies.apply(convert_salary, axis=1, cb=CB)
+
+vacancies['salary_bin'] = vacancies.apply(calc_salary_bin, axis=1)
+
+vacancies.to_csv('data/vacancies_bi.csv', index=False)
--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@ import re
 from tqdm import tqdm
 from time import sleep
 import pandas as pd
-from typing import List, Optional
+import numpy as np
+from typing import List
+from pymystem3 import Mystem
 
 
 def get_ids_on_page(url: str) -> List[int]:
@@ -97,6 +99,235 @@ def get_dataset(ids: List[int]) -> pd.DataFrame:
                'employment', 'description', 'salary_from', 'salary_to', 'currency_salary']
     return pd.DataFrame(dataset, columns=columns)
 
+
+def calc_experience(value: str) -> str:
+    """
+    Определяет уровень опыта на основе переданной строки.
+
+    Функция анализирует строку, содержащую информацию об опыте работы, 
+    и возвращает квалификационный уровень в соответствии с найденными данными.
+    Если опыт работы не указан, возвращает уровень 'Junior (no experience)'.
+
+    Parameters:
+    value (str): Строка, содержащая информацию об опыте работы.
+
+    Returns:
+    str: Квалификационный уровень на основе опыта работы.
+    """
+
+    # Используем регулярное выражение для поиска цифр в строке
+    experience = re.findall(r'\d', value)
+
+    # Если цифры не найдены, считаем, что опыт работы отсутствует
+    if not experience:
+        return 'Junior (no experience)'
+    
+    # Возвращаем соответствующий уровень опыта на основе найденной первой цифры
+    if experience[0] == '6':
+        return 'Senior (6+ years)'
+    if experience[0] == '1':
+        return 'Junior+ (1-3 years)'
+    if experience[0] == '3':
+        return 'Middle (3-6 years)'
+    
+
+def lemmatize_corpus(description: pd.Series) -> pd.Series:
+    """
+    Производит лемматизацию серии текстовых данных.
+
+    Эта функция принимает на вход серию текстовых данных (pd.Series), 
+    объединяет их в один текст, разделяя маркером ' br ', 
+    затем применяет к нему лемматизацию и возвращает новую серию, 
+    где каждый элемент представляет собой лемматизированный текст.
+
+    Parameters:
+    description (pd.Series): Серия текстовых данных для лемматизации.
+
+    Returns:
+    pd.Series: Серия лемматизированных текстовых данных.
+    """
+    
+    # Объединение всех текстов в одну строку с маркером ' br ' для разделения текстов
+    texts = ' br '.join(description.to_list())
+    
+    # Инициализация лемматизатора
+    stem = Mystem()
+    
+    print('Запуск лемматизации')
+    # Применение лемматизации к объединенному тексту
+    text_lemm = stem.lemmatize(texts)
+    
+    # Инициализация списка для хранения лемматизированных текстов
+    data = []
+    # Временный список для хранения слов текущего текста
+    temp = []
+    
+    # Проходим по всем словам в лемматизированном тексте
+    for word in tqdm(text_lemm):
+        # Если встречаем маркер 'br', значит это конец текущего текста
+        if word == 'br':
+            # Добавляем лемматизированный текст в список data
+            data.append(' '.join([word for word in temp if word.isalpha()]))
+            # Очищаем временный список для следующего текста
+            temp = []
+        else:
+            # Добавляем слово в временный список
+            temp.append(word)
+    # Добавляем последний текст в список data
+    data.append(' '.join([word for word in temp if word.isalpha()]))
+    
+    # Преобразуем список лемматизированных текстов в pd.Series
+    data = pd.Series(data, name='description_lemmatized')
+    
+    # Проверка на соответствие размеров входной и выходной серий
+    assert description.shape[0] == data.shape[0]
+    
+    return data
+
+
+def calc_skills_from_description(value: str, skills) -> str:
+    """
+    Извлекает навыки из описания, используя заданный список навыков SKILLS.
+
+    Функция проходит по словам в строке 'value', объединяет их в группы от одного до четырех слов,
+    и если сочетание слов является навыком из списка SKILLS, добавляет его в результат.
+    Результат возвращает в виде строки, где навыки разделены запятой.
+
+    Parameters:
+    value (str): Строка с описанием, из которой необходимо извлечь навыки.
+
+    Returns:
+    str: Строка с уникальными навыками, извлеченными из описания.
+    """
+    res = []  # Инициализация списка для хранения найденных навыков
+    value = value.lower().split()  # Приведение строки к нижнему регистру и разделение на слова
+    start = 0  # Начальный индекс для среза слов
+    stop = 4  # Конечный индекс для среза слов
+
+    # Пока конечный индекс не превысит количество слов в строке
+    while stop <= len(value):
+        skill = ' '.join(value[start:stop])  # Объединение слов в потенциальный навык
+        if skill in skills:  # Проверка, является ли сочетание слов навыком
+            res.append(skill)  # Добавление навыка в результат
+            start = stop  # Сдвиг начального индекса
+            stop = start + 4  # Сдвиг конечного индекса
+        else:
+            # Уменьшение конечного индекса для проверки следующего сочетания слов
+            if start < stop - 1:
+                stop -= 1
+            else:
+                # Сдвиг начального индекса, если не найдено сочетаний
+                start = stop
+                stop = start + 4
+    return ', '.join(set(res))  # Возврат строки с уникальными навыками
+
+def calc_skills(row: dict) -> str:
+    """
+    Вычисляет итоговый набор навыков из двух полей строки DataFrame: 'skills_from_description' и 'skills_from_key_skills'.
+
+    Если одно из полей пустое, возвращает значение другого поля. Если оба поля содержат значения,
+    объединяет их в один уникальный набор навыков.
+
+    Returns:
+    str: Строка с уникальными навыками, полученными из обоих полей.
+    """
+    # Проверка и возврат навыков из описания, если поле с навыками из описания пусто
+    if row['skills_from_description'] == '':
+        return row['skills_from_key_skills']
+    # Проверка и возврат навыков из ключевых слов, если поле с навыками из ключевых навыков пусто
+    if row['skills_from_key_skills'] == '':
+        return row['skills_from_description']
+    # Объединение и возврат уникальных навыков из обоих полей
+    return ', '.join(set(row['skills_from_description'].split(', ') + row['skills_from_key_skills'].split(', ')))
+
+
+def calc_salary_num(row: pd.Series) -> float:
+    """
+    Вычисляет среднее значение зарплаты на основе верхнего и нижнего порогов.
+
+    Функция принимает строку DataFrame, содержащую информацию о зарплатных предложениях
+    (salary_to и salary_from), и возвращает среднее значение, если указаны оба порога,
+    или одно из значений, если указан только один порог.
+
+    Parameters:
+    row (pd.Series): Строка DataFrame, содержащая информацию о зарплатных предложениях.
+
+    Returns:
+    float: Среднее значение зарплаты или один из указанных порогов.
+    """
+    
+    # Проверяем, указаны ли оба порога зарплаты, и если да, возвращаем их среднее
+    if not row.isna()['salary_to'] and not row.isna()['salary_from']:
+        return (row['salary_to'] + row['salary_from']) / 2
+    
+    # Возвращаем верхний порог зарплаты, если он указан
+    if not row.isna()['salary_to']:
+        return row['salary_to']
+    
+    # Возвращаем нижний порог зарплаты, если он указан
+    if not row.isna()['salary_from']:
+        return row['salary_from']
+    
+
+def convert_salary(row: pd.Series, cb: dict) -> float:
+    """
+    Конвертирует зарплату из валюты вакансии в рубли на основе курса ЦБ.
+
+    Функция проверяет валюту зарплаты и конвертирует её в рубли, используя
+    текущий курс Центрального Банка, содержащийся в словаре cb.
+
+    Parameters:
+    row (pd.Series): Строка DataFrame, содержащая информацию о зарплате и валюте.
+    cb (dict): Словарь с данными о курсах валют от Центрального Банка.
+
+    Returns:
+    float: Зарплата в рублях после конвертации.
+    """
+
+    # Если валюта указана в рублях вернем это значение
+    if row['currency_salary'] == 'RUR' or row['currency_salary'] is np.nan:
+        return row['salary_num']
+    
+    # Если валюта зарплаты указана как BYR, конвертируем её в BYN
+    if row['currency_salary'] == 'BYR':
+        row['currency_salary'] = 'BYN'
+    
+    # Получаем курс валюты из словаря CB
+    dic = cb['Valute'][row['currency_salary']]
+    rates = dic['Value'] / dic['Nominal']
+    
+    # Возвращаем зарплату в рублях после конвертации
+    return row['salary_num'] * rates
+
+
+def calc_salary_bin(row: pd.Series) -> str:
+    """
+    Категоризирует зарплатные предложения вакансий по их размеру.
+
+    Функция принимает строку DataFrame, содержащую информацию о зарплате (salary_to и salary_from),
+    и возвращает категорию зарплаты в зависимости от указанных значений.
+
+    Parameters:
+    row (pd.Series): Строка DataFrame, содержащая информацию о зарплатных предложениях.
+
+    Returns:
+    str: Категория зарплаты.
+    """
+    
+    # Проверяем отсутствие данных о зарплате
+    if row.isna()['salary_rub']:
+        return 'ЗП не указана'
+    
+    # Категоризируем зарплату
+    else:
+        if row['salary_rub'] > 3e5:
+            return 'Больше 300 тысяч'
+        if row['salary_rub'] > 2e5:
+            return 'От 200 тысяч до 300 тысяч'
+        if row['salary_rub'] > 1e5:
+            return 'От 100 тысяч до 200 тысяч'
+        else:
+            return 'Меньше 100 тысяч'
     
     
     
